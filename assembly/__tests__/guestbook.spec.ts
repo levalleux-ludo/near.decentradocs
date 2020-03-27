6bb40fbbfcc494c0b4d83c787296ffd9c8233257
Ludovic Levalleux <levalleux_ludo@hotmail.com>
2020-03-27 19:50:52 +0000

    1st test OK

--- assembly/__tests__/guestbook.spec.ts
+++ assembly/__tests__/guestbook.spec.ts
@@ -1,5 +1,5 @@
-import { docExists, registerDoc, getAuthorizedAccounts } from '../main'
-import { PersistentVector, context } from 'near-sdk-as'
+import { docExists, registerDoc, getAuthorizedAccounts, getAuthor, init } from '../main'
+import { PersistentVector, context, Context } from 'near-sdk-as'
 // import { v4 as uuid } from 'uuid';
 
 // const doc1 = {
@@ -8,23 +8,31 @@ import { PersistentVector, context } from 'near-sdk-as'
 //   subscriptionFee: 123456,
 //   authorized: new PersistentVector<string>("authorized_doc1:")
 // };
+const account0 = 'account0';
 const docId1: string = '1111';
 const encryptedKey1: string = 'xxxxx';
 const subscriptionFee1: u64 = 123456;
 // const authorized1 = new PersistentVector<string>("authorized_doc1:");
-const authorized1: string[] = [];
+const authorized1: string[] = ['toto', 'tata', 'titi'];
 const docId2: string = '2222';
 const encryptedKey2: string = 'xxxxx';
 const subscriptionFee2: u64 = 0;
 const authorized2 = new PersistentVector<string>("authorized_doc2:");
 
 describe('Test DVSRegistry contract', () => {
+  beforeAll(() => {
+    Context.setSigner_account_id(account0);
+    init(account0);
+  });
+
   it('be able to register a protected document', () => {
     expect(docExists(docId1)).toBe(false);
     expect(docExists(docId2)).toBe(false);
     registerDoc(docId1, encryptedKey1, subscriptionFee1, authorized1);
-    // expect(docExists(docId1)).toBe(true);
-    // expect(docExists(docId2)).toBe(false);
-
+    expect(docExists(docId1)).toBe(true);
+    expect(docExists(docId2)).toBe(false);
+    let authorized = getAuthorizedAccounts(docId1);
+    expect(authorized.length).toBe(authorized1.length);
+    expect(getAuthor(docId1)).toBe(account0);
   })
 })
--- assembly/main.ts
+++ assembly/main.ts
@@ -1,21 +1,58 @@
 // @nearfile
 
-import { PersistentVector } from "near-sdk-as";
+import { PersistentVector, PersistentMap, context, logging, storage } from "near-sdk-as";
 
 // --- contract code goes below
+let _initialOwner: string;
+const _authorPerDocId = new PersistentMap<string, string>("authors:");
+// const _authorizedAddressPerDocId = new PersistentMap<string, PersistentVector<string>>("authorizedVector:")
+const _authorizedAddressPerDocId = new PersistentMap<string, Array<string>>("authorizedVector:")
+const _subscriptionFeePerDocId = new PersistentMap<string, u64>("subscriptionFee:")
+const _encryptedKeyPerDocId = new PersistentMap<string, string>("encryptedKey:")
+
+export function init(initialOwner: string): void {
+  logging.log("initialOwner: " + initialOwner);
+  assert(storage.get<string>("init") == null, "Already initialized token supply");
+  _initialOwner = initialOwner;
+  storage.set("init", "done");
+}
 
 export function docExists(docId: string): bool {
-  return false;
+  return _authorPerDocId.contains(docId);
 }
 
 export function registerDoc(
   docId: string,
   encryptedKey: string,
   subscriptionFee: u64,
-  authorizedAddresses: string[] ): void {
+  authorizedAddresses: Array<string> ): void {
+    assert(!docExists(docId), 'a document with this id already exists');
+    const author: string = context.sender;
+    _authorPerDocId.set(docId, author);
+    _encryptedKeyPerDocId.set(docId, encryptedKey);
+    _subscriptionFeePerDocId.set(docId, subscriptionFee);
+    // const authorizedVector = new PersistentVector<string>("authorized:");
+    // for (let i = 0; i < authorizedAddresses.length; i++) {
+    //   authorizedVector.push(authorizedAddresses[i]);
+    // }
+    // _authorizedAddressPerDocId.set(docId, authorizedVector);
+    _authorizedAddressPerDocId.set(docId, authorizedAddresses);
 }
 
-export function getAuthorizedAccounts(docId: string): string[] {
-  return [];
+export function getAuthorizedAccounts(docId: string): Array<string> {
+  const authorized = _authorizedAddressPerDocId.get(docId);
+  if (!authorized) {
+    return [];
+  }
+  const result = new Array<string>(authorized.length);
+  for (let i = 0; i < authorized.length; i++) {
+    result[i] = authorized[i];
+  }
+  return result;
+}
+
+export function getAuthor(docId: string): string | null {
+  assert(docExists(docId), "this document has not been registered");
+  return _authorPerDocId.get(docId);
 }
 
